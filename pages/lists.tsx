f99ee8b42c13e1f018f5345de7b88f49c62efa7f
Md Shadman Matin (Piyal) <78369801+YamatoDX@users.noreply.github.com>
2022-06-05 07:52:50 +0600

    export style updated for lists page component

--- pages/lists.tsx
+++ pages/lists.tsx
@@ -7,8 +7,8 @@ const API = `/api/entries`;
 const options = {
   method: "GET",
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 };
 const fetcher = async (url: string) => {
   const res = await fetch(url, options);
@@ -16,56 +16,33 @@ const fetcher = async (url: string) => {
   if (!res.ok) {
     throw new Error(`That's an error`);
   }
-
-  /*interface dataType {
-    id: string;
-    title: string;
-    details: string;
-    category: string;
-  }*/
-
   const data = await res.json();
 
   return data;
 };
 
-const lists = () => {
+export default function Lists() {
   const { data, error } = useSWR(API, fetcher);
   console.log(`Data: `, data);
 
   if (error) {
-    return (
-      <Alert>Loading Failed: {error.message}</Alert>
-    );
+    return <Alert>Loading Failed: {error.message}</Alert>;
   }
 
   if (!data) {
-    return (
-      <Alert>Loading...</Alert>
-    );
+    return <Alert>Loading...</Alert>;
   }
 
   return (
     <Container>
       <h1>Lists</h1>
-      <Grid 
-        container
-        spacing={3}
-      >
+      <Grid container spacing={3}>
         {data.map((note) => (
-          <Grid 
-            key={note.id}
-            item
-            xs={12}
-            md={6}
-            lg={4}
-          >
+          <Grid key={note.id} item xs={12} md={6} lg={4}>
             <NoteLists note={note}></NoteLists>
           </Grid>
         ))}
       </Grid>
     </Container>
   );
-};
-
-export default lists;
+}
